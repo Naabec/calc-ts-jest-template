acb315af77bd0985577ee68ed0440cadaaadb9db
aleksei.golov <lelik.llk@gmail.com>
2024-10-04 15:21:33 +0300

    TSK 2 - Added 2 new methods and tests

--- src/processor.ts
+++ src/processor.ts
@@ -1,11 +1,16 @@
 import { User } from './user'
 
 export class Processor {
-
   giveConsent(user: User): void {
     user.consentGiven = true
-}
- checkConsent(user: User) : boolean {
+  }
+  checkConsent(user: User): boolean {
     return user.consentGiven === true
- }
+  }
+  checkUserAge(user: User): boolean {
+    return user.age !== undefined && user.age > 18;
+  }
+  revokeConsent(user: User): void {
+    user.consentGiven = false
+  }
 }
--- test/consent.test.ts
+++ test/consent.test.ts
@@ -2,24 +2,42 @@ import { User } from '../src/user'
 import { Processor } from '../src/processor'
 
 describe('Tests for consent ', () => {
-  let user1 : User
-  let processor1 : Processor
-  beforeEach(() =>{
-    user1 = new User('Aleksei','Golov','+37256710032','Kollane 8 - 34')
+  let user1: User
+  let processor1: Processor
+  beforeEach(() => {
+    user1 = new User('Aleksei', 'Golov', 0, '+37256710032', 'Kollane 8 - 34')
     processor1 = new Processor()
   })
-  test('Not processed user should initially have undefined consent', () =>{
-
+  test('Not processed user should initially have undefined consent', () => {
     expect(user1.consentGiven).toBeUndefined()
   })
-  test('Processor can give consent to user', () =>{
-
+  test('Processor can give consent to user', () => {
     processor1.giveConsent(user1)
     expect(user1.consentGiven).toBe(true)
   })
 
-  test('Not processed user should initially have undefined consent', () =>{
-
+  test('Not processed user should initially have undefined consent', () => {
     expect(processor1.checkConsent(user1)).toBe(false)
+  })
+
+  test('Should not give consent if user is younger than 18', () => {
+    user1.age = 17
+    expect(processor1.checkUserAge(user1)).toBe(false)
+  })
+
+  test('Should give consent if user is older than 18', () => {
+    user1.age = 20
+    const result = processor1.checkUserAge(user1)
+
+    expect(result).toBe(true)
+  })
+
+  test('Should revoke consent', () => {
+    user1.age = 20
+    processor1.giveConsent(user1)
+    expect(user1.consentGiven).toBe(true)
+
+    processor1.revokeConsent(user1)
+    expect(user1.consentGiven).toBe(false)
   })
 })
